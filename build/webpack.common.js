a44d12b748a6d63539f40be9ca1fe6110403261e
fuzong.wang <fuzong.wang@ikang.com>
2018-10-10 14:48:09 +0800

    update

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -1,67 +1,79 @@
 const webpack = require('webpack');
-const path = require("path");
+const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const PrerenderSPAPlugin = require('prerender-spa-plugin')
-
+const PrerenderSPAPlugin = require('prerender-spa-plugin');
+const Renderer = PrerenderSPAPlugin.PuppeteerRenderer;
 
 module.exports = {
-	entry: {
-		app: path.join(__dirname, '../src/index.js')
-	},
-	output: {
-		filename: '[name].js',
-		path: path.join(__dirname, '../dist'),
-		sourceMapFilename: '[name].map'
-	},
-	resolve: {
-		extensions: ['.js', '.vue', '.json', '.css', '.scss'],
-		modules: [path.join(__dirname, '../src'), 'node_modules'],
-		alias: {
-			vue: 'vue/dist/vue.esm.js'
-		}
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				loader: 'babel-loader',//需安装 babel-plugin-syntax-dynamic-import 插件用于支持 ()=>import('xxx')
-			},
-			{
-				test: /\.vue$/,
-				loader: 'vue-loader',
-				options: {
-					// extractCSS: process.env.NODE_ENV === 'production',
-				}
-			},
-			{
-				test: /\.(png|svg|jpg|gif)$/,
-				loader: 'file-loader',
-				options: {
-					name: 'img/[name].[hash].[ext]'
-				}
-			}
-		]
-	},
-	//devtool:'inline-source-map',//会一直生成map
-	devtool: '#cheap-module-source-map',//当 NODE_ENV=production 时，只会map文件框架~
-	plugins: [
-		// 主要是为了把robots.txt 拷到根目录下
-	    new CopyWebpackPlugin([
-	      {
-	        from: path.join(__dirname, '../src/static/forRoot'),
-	        to: path.join(__dirname, '../dist'),
-	        ignore: ['.*']
-	      }
-	    ]),
-		new webpack.ProvidePlugin({
-			$: 'jquery',
-			jQuery: 'jquery'
+  entry: {
+    app: path.join(__dirname, '../src/index.js')
+  },
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, '../dist'),
+    sourceMapFilename: '[name].map'
+  },
+  resolve: {
+    extensions: ['.js', '.vue', '.json', '.css', '.scss'],
+    modules: [path.join(__dirname, '../src'), 'node_modules'],
+    alias: {
+      vue: 'vue/dist/vue.esm.js'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader' //需安装 babel-plugin-syntax-dynamic-import 插件用于支持 ()=>import('xxx')
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          // extractCSS: process.env.NODE_ENV === 'production',
+        }
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        loader: 'file-loader',
+        options: {
+          name: 'img/[name].[hash].[ext]'
+        }
+      }
+    ]
+  },
+  //devtool:'inline-source-map',//会一直生成map
+  devtool: '#cheap-module-source-map', //当 NODE_ENV=production 时，只会map文件框架~
+  plugins: [
+    // 主要是为了把robots.txt 拷到根目录下
+    new CopyWebpackPlugin([
+      {
+        from: path.join(__dirname, '../src/static/forRoot'),
+        to: path.join(__dirname, '../dist'),
+        ignore: ['.*']
+      }
+    ]),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
     }),
+    // == PRERENDER SPA PLUGIN == //
     new PrerenderSPAPlugin({
-      // Required - The path to the webpack-outputted app to prerender.
-      staticDir: path.join(__dirname, '../dist'),
-      // Required - Routes to render.
-      routes: [ '/', '/about' ]
+      // Index.html is in the root directory.
+      staticDir: path.join(__dirname, '../dist/'),
+      routes: [ '/' ],
+      // Optional minification.
+      minify: {
+        collapseBooleanAttributes: true,
+        collapseWhitespace: true,
+        decodeEntities: true,
+        keepClosingSlash: true,
+        sortAttributes: true
+      },
+
+      renderer: new Renderer({
+        renderAfterDocumentEvent: 'render-event'
+      })
     })
-	]
-}
+  ]
+};
--- src/index.js
+++ src/index.js
@@ -1,29 +1,28 @@
 require('es6-promise/auto');
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import routerConfig from 'router'
-import Layout from './components/Layout.vue'
-import store from './store'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import routerConfig from 'router';
+import Layout from './components/Layout.vue';
+import store from './store';
 
-import myStorage from './Storage'
+import myStorage from './Storage';
 
+import VueCookies from 'vue-cookies'; //https://www.npmjs.com/package/vue-cookies
+Vue.use(VueCookies);
 
-import VueCookies from 'vue-cookies'//https://www.npmjs.com/package/vue-cookies
-Vue.use(VueCookies)
+Vue.use(VueRouter);
+const router = new VueRouter(routerConfig);
 
-Vue.use(VueRouter)
-const router = new VueRouter(routerConfig)
-
-require('./scss/_reset.scss')
+require('./scss/_reset.scss');
 
 new Vue({
   router,
   store,
-  el:"#app",
+  el: '#app',
   render: h => h(Layout),
-  mounted:function(){
-      store.commit("initToken")
-    },
-  destroyed:()=>{
-  }
-})
+  mounted: function() {
+    store.commit('initToken');
+    document.dispatchEvent(new Event('render-event'));
+  },
+  destroyed: () => {}
+});
