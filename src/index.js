6fa6b3566108910aff2a588d6923c25a992383b6
fuzong.wang <fuzong.wang@ikang.com>
2018-10-10 17:46:36 +0800

    update 6

--- src/index.js
+++ src/index.js
@@ -23,7 +23,9 @@ new Vue({
   mounted: function() {
     store.commit('initToken');
     // document.dispatchEvent(new Event('render-event'));
-    setTimeout(() => { document.dispatchEvent(new Event('render-event')); }, 5000);
+    setTimeout(() => {
+      document.dispatchEvent(new Event('render-event'));
+    }, 5000);
     // this.$nextTick(()=>{
     //   document.dispatchEvent(new Event('render-event'));
     // })
